1739e0f31b707988eca36fef59cf05f45d1e46c3
Zack Bostian <bostianz@gmail.com>
2016-05-20 16:14:14 -0400

    added images to home page, and routes for future pages

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,8 +3,25 @@ import React, { Component } from 'react';
 export default class HomePage extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="homePage">
+        <div className="homePage__HeroLink edge--bottom">
+          <img src="http://www.planwallpaper.com/static/images/image-159225.jpg" alt="WOW" />
+        </div>
+        <div className="homePage__HeroLink edge--bottom">
+          <img src="http://i.kinja-img.com/gawker-media/image/upload/utsyr3blwnesou8aqyes.jpg" alt="Diablo 3" />
+        </div>
+        <div className="homePage__HeroLink edge--bottom">
+          <img src="http://blogs-images.forbes.com/insertcoin/files/2014/12/hearthstone1.jpg" alt="Hearthstone" />
+        </div>
+        <div className="homePage__HeroLink">
+          <img src="http://i.imgur.com/PCyz3b5.jpg" alt="Overwatch" />
+        </div>
+        <div className="homePage__HeroLink">
+          <img src="https://assets.vg247.com/current//2014/11/Starcraft2-logo.jpg" alt="Starcraft 2" />
+        </div>
+        <div className="homePage__HeroLink">
+          <img src="http://www.tacticalgaming.net/hq/news/wp-content/uploads/2015/10/heroes-facebook-preview.jpg" alt="Hereos of the Storm" />
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import Promise from 'redux-promise';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 // CONTAINERS
 import App from './app/app';
+import Home from './components/HomePage';
 //REDUCERS
 import rootReducer from './reducers/RootReducer';
 // Store with middleware.
@@ -23,7 +24,17 @@ const store = createStoreWithMiddleware(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={Home}/>
+        <Route path="wow" component={Wow} />
+        <Route path="diablo3" component={Diablo} />
+        <Route path="hearthstone" component={Hearthstone} />
+        <Route path="overwatch" component={Overwatch} />
+        <Route path="starcraft2" component={Starcraft} />
+        <Route path="hots" component={Heroes} />
+      </Route>
+    </Router>
   </Provider>,
   document.querySelector('.container')
 );
